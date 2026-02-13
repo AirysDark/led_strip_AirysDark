9adb34d7ee2e249539fc4893022becd65d69fc43
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-02-13 12:39:51 +1100

    Add predefined RGB color constants

---
+++ src/color.c
@@ -0,0 +1,13 @@
+#include "color.h"
+
+// --------------------------------------------------
+// Predefined RGB colors (flash-resident, zero RAM cost)
+// --------------------------------------------------
+const rgb_t COLOR_RED     = { 255,   0,   0 };
+const rgb_t COLOR_GREEN   = {   0, 255,   0 };
+const rgb_t COLOR_BLUE    = {   0,   0, 255 };
+const rgb_t COLOR_WHITE   = { 255, 255, 255 };
+const rgb_t COLOR_BLACK   = {   0,   0,   0 };
+const rgb_t COLOR_YELLOW  = { 255, 255,   0 };
+const rgb_t COLOR_CYAN    = {   0, 255, 255 };
+const rgb_t COLOR_MAGENTA = { 255,   0, 255 };
